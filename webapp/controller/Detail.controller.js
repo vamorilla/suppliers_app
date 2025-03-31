0519201068b92114b11d72403e774c654d7f26d3
Vanesa Morilla <vamorilla@gmail.com>
2025-03-31 16:06:20 +0000

    Added detail files, routing in manifest, navigation on suppliers table and neccesary configs

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.bootcamp.sapui5.suppliersapp.controller.Detail", {
+        onInit() {
+            let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            let sSupplierID = oEvent.getParameter("arguments").ProductID;
+
+            this.getView().bindElement({
+                path: "/Suppliers(" + sSupplierID + ")",
+                parameters: {
+                    expand: "Products"
+                }
+            });
+        },
+    });
+});
