26d2b859e2ff7813c1a8d1ab513b7b823ea06f96
Vanesa Morilla <vamorilla@gmail.com>
2025-03-31 00:08:42 +0000

    Table ui implemented with sorting funcionalities

--- webapp/controller/Suppliers.controller.js
+++ webapp/controller/Suppliers.controller.js
@@ -1,10 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Sorter",
+    "sap/ui/core/library"
+], (Controller, Sorter, CoreLibrary) => {
     "use strict";
+
+    const SortOrder = CoreLibrary.SortOrder;
 
     return Controller.extend("com.bootcamp.sapui5.suppliersapp.controller.Suppliers", {
         onInit() {
-        }
+        },
+
+        clearAllSortings: function() {
+			const oTable = this.byId("table");
+			oTable.getBinding().sort(null);
+			this._resetSortingState();
+		},
+
+        _resetSortingState: function() {
+			const oTable = this.byId("table");
+			const aColumns = oTable.getColumns();
+			for (let i = 0; i < aColumns.length; i++) {
+				aColumns[i].setSortOrder(SortOrder.None);
+			}
+		}
     });
 });
